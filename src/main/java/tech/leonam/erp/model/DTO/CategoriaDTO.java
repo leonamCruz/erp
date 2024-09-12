72bc690d5ed6aaa343d359d188c7b2f1c535f1e1
usuario <u@u.c>
2024-09-12 16:57:28 -0300

    Add @

--- src/main/java/tech/leonam/erp/model/DTO/CategoriaDTO.java
+++ src/main/java/tech/leonam/erp/model/DTO/CategoriaDTO.java
@@ -1,7 +1,12 @@
 package tech.leonam.erp.model.DTO;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class CategoriaDTO {
+    @NotBlank
     private String nome;
+    @NotBlank
     private String descricao;
+    @NotBlank
     private Boolean ativo;
 }
