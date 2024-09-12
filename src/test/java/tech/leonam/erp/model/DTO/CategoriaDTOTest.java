3ab025f674352d3e92572bdfea607b83451f90a6
usuario <u@u.c>
2024-09-12 17:28:53 -0300

    Add Pessoa Fake

---
+++ src/test/java/tech/leonam/erp/model/DTO/CategoriaDTOTest.java
@@ -0,0 +1,20 @@
+package tech.leonam.erp.model.DTO;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+@Test
+class CategoriaDTOTest {
+
+    @BeforeEach
+    void setup(){
+
+    }
+
+    @Test
+    void DTOCorreto(){
+        CategoriaDTO dto = new CategoriaDTO();
+
+    }
+
+}
